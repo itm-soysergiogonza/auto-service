2ddf32bab6ed8d1e6441b7c6ce9a69ae261586df
Sergio González Sánchez <sergiogonzalez@itm.edu.co>
2025-02-17 17:04:08 -0500

    add:  build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-// ... otros imports ...
+import { provideHttpClient } from '@angular/common/http';
+import {AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // ... otros componentes ...
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
